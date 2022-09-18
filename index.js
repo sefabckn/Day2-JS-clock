10ce588155a684d81149131aa98fa078e35e2874
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2022-09-18 20:07:34 +0100

    Files  imported

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload =(e) => {
+    e.preventDefault()
+    const secondHand = document.querySelector('.second-hand')
+    const minHand = document.querySelector('.min-hand')
+    const hourHand = document.querySelector('.hour-hand')
+    function setDate() {
+      const now = new Date()
+      const seconds = now.getSeconds();
+      const secondDegrees = ((seconds / 60) * 360) + 90
+      secondHand.style.transform = `rotate(${secondDegrees}deg)`
+
+
+      const mins = now.getMinutes();
+      const minsDegrees = ((mins / 60) * 360) + 90;
+      minHand.style.transform = `rotate(${minsDegrees}deg)`
+
+      const hour = now.getHours();
+      console.log(hour)
+      const hourDegrees = ((hour / 12) * 360) + 90;
+      hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
+    }
+    setInterval(setDate, 1000)
+}

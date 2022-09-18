1a4c05b416ade6685825a147fd0a97a183293fdd
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2022-09-18 22:15:42 +0100

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ window.onload =(e) => {
       const minsDegrees = ((mins / 60) * 360) + 90;
       minHand.style.transform = `rotate(${minsDegrees}deg)`
 
-      const hour = now.getHours();
+      const hour = now.getHours() +1 ;
       console.log(hour)
       const hourDegrees = ((hour / 12) * 360) + 90;
       hourHand.style.transform = `rotate(${hourDegrees}deg)`
